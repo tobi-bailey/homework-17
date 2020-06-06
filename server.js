428f2ace3b0addf37484b344a92b489ab3c992db
Tobi Bailey <baileygtobi@gmail.com>
2020-06-06 19:49:10 -0400

    added models and routes files

--- server.js
+++ server.js
@@ -1,23 +1,55 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static("public"));
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+// // routes
+// app.use(require("./public/api.js"));
+
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
+
+// Dependencies
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
+// Setting up Express App
 const app = express();
+const PORT = process.env.PORT || 8000;
 
+app.use(morgan("dev"));
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static("public"));
+// db mongo
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// Creating Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes.js")(app);
 
-// routes
-app.use(require("./public/api.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+// Starts the server to begin listening
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
 });
